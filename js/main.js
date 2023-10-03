cee83d0c25a3dfd7259bb9da822b12e830c31fde
dalia2323 <d.k.saleem@students.ptuk.edu.ps>
2023-10-03 20:27:48 +0300

    dark mode #2

--- js/main.js
+++ js/main.js
@@ -30,15 +30,15 @@ function displayCountries() {
       if ((!searchValue || country.name.common.toLowerCase().includes(searchValue)) &&
           (categoryValue=='no filter' ||categoryValue=='filter by region' || country.region.toLowerCase().includes(categoryValue))) {
         result += `
-          <div class="col-lg-3 col-md-4 col-sm-6 mt-sm-2 mb-3 rounded-1">
+          <div class="col-lg-3 col-md-4 col-sm-6 mt-sm-2 mb-3 rounded-1 ">
             <a href="country/${country.name.common.toLowerCase()}.html">
-              <div class="card  w-width shadow-sm">
-                <img src='${country.flags.svg}' class="card-img-top" alt="...">
+              <div class="card  w-width shadow-sm card-style">
+                <img src='${country.flags.svg}' class="card-img-top" alt="..." >
                 <div class="card-body">
                   <h6 class="card-title">${country.name.common}</h6>
                   <p class="card-text"><b>Population</b>: ${country.population}</p>
                   <p class="card-text"><b>Region</b>: ${country.region}</p>
-                  <p class="card-text"><b>Capital</b>: ${country.capital[0]}</p>
+                  <p class="card-text mb-4"><b>Capital</b>: ${country.capital[0]}</p>
                 </div>
                 </a>
               </div>
@@ -52,12 +52,21 @@ function displayCountries() {
   
 ////////////////////////
 function toggleDarkMode() {
-    const body = document.body;
-    const container = document.getElementById('container');
-    body.classList.toggle("dark-mode");
-    container.classList.toggle("dark-mode");
+  const body = document.body;
+  const container = document.getElementById('container');
+  const darkModeIcon = document.getElementById('darkModeIcon');
+
+  body.classList.toggle("dark-mode");
+  container.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+      darkModeIcon.classList.remove("fa-moon");
+      darkModeIcon.classList.add("fa-sun");
+  } else {
+      darkModeIcon.classList.remove("fa-sun");
+      darkModeIcon.classList.add("fa-moon");
+  }
 }
 
 const darkModeToggle = document.getElementById("darkModeToggle");
 darkModeToggle.addEventListener("click", toggleDarkMode);
-
